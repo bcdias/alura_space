b72f86f3cda0898fdecbccea8bf4fc0ddac0a794
Bruno Dias <dias.desenvolvedor@gmail.com>
2022-10-21 13:11:19 -0300

    Criando página de imagem

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,8 @@
+from tkinter import N
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
     path('', index),
+    path('imagem/', imagem, name='imagem')
 ]
